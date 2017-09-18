4f79d9ab2774b04574abfdcd95d4e294cf0080b6
aktunc-STMU <aktunc@gmail.com>
2017-09-18 14:56:42 -0500

    Looks good, check comment

--- TestGeometricObject.java
+++ TestGeometricObject.java
@@ -4,7 +4,7 @@ public class TestGeometricObject {
 		GeometricObject1 b = new Circle(0);
 		GeometricObject1 a = new Rectangle(0,0);
 		
-		System.out.println("The Two Objects have the same area? " + ((Circle) b).isEquals(b,a));
+		System.out.println("The Two Objects have the same area? " + ((Circle) b).isEquals(b,a)); //This is fine. To avoid casting, you can define b as Circle object.
 		System.out.println("Does your Object have a Greater Area? " + ((Circle) b).isGreater(b,a));
 		System.out.println("Does your Object have a Lesser Area? " + ((Circle) b).isLess(b,a));
 		
