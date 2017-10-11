61ddf1ba16b08df782307c49aa46e829ebb54723
GreatBrando <bboyfranco@att.net>
2017-10-11 17:36:19 -0500

    Add files via upload

---
+++ Scores.java
@@ -0,0 +1,30 @@
+
+public class Scores {
+	
+	private int scores[];
+	
+	public int[] getScores() {
+		return scores;
+	}
+
+	public void setScores(int[] scores) {
+		this.scores = scores;
+	}
+
+	public Scores(int[] scores){
+		this.scores = scores;
+	}
+	
+	public int getAverage(){
+		int sum = 0;
+		
+		for(int score: scores){
+			if(score< 0 || score>100){
+				throw new IllegalArgumentException("Score Invalid");
+			}
+			sum += score;
+		}
+		return sum/scores.length;
+	}
+	
+}
